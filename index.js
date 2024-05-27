0f161784bd4944611efdf089e2609bd948a7d866
Yidy <jetfung060@gmail.com>
2024-05-27 12:15:58 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+// Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // 400 (game width) - 40 (dodger width)
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
